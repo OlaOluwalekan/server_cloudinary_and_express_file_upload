c8688f9c8a389f63dcc2298c472f3af85cf6857e
OlaOluwalekan <olalekanbello534@gmail.com>
2024-01-16 19:41:00 +0100

    added database

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const express = require('express')
 const fileUpload = require('express-fileupload')
 const cloudinary = require('cloudinary').v2
 const cors = require('cors')
+const mongoose = require('mongoose')
+
+const connect_string = process.env.MONGO_URI
+
+mongoose.connect(connect_string)
 
 const app = express()
 
@@ -17,6 +22,13 @@ app.use(
 )
 app.use(fileUpload())
 app.use(express.json())
+
+const imageModel = mongoose.model(
+  'images',
+  new mongoose.Schema({
+    image: String,
+  })
+)
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -54,7 +66,8 @@ app.post('/uploads', uploadFile, async (req, res) => {
     const clo = await cloudinary.uploader.upload(req.image, {
       folder: 'Bells Arena/test',
     })
-    res.status(200).json({ msg: 'file uploaded', link: clo.secure_url })
+    const image = await imageModel.create({ image: clo.secure_url })
+    res.status(200).json({ msg: 'file uploaded', image })
   } catch (error) {
     res.status(500).json(error)
   }
